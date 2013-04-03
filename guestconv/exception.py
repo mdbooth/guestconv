30790e53f52de69d5283a4a2ba83127a61a68a45
Matthew Booth <mbooth@redhat.com>
2013-04-03 17:09:32 +0100

    Add some more exceptions, and give them a common base class

--- guestconv/exception.py
+++ guestconv/exception.py
@@ -17,5 +17,8 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-class UnsupportedConversion(Exception): pass
-class InvalidConversion(Exception): pass
+class GuestConvException(Exception): pass
+
+class UnsupportedConversion(GuestConvException): pass
+class InvalidConversion(GuestConvException): pass
+class ConversionError(GuestConvException): pass
