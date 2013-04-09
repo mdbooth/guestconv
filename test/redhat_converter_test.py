d4b1e5dbba338d87e28b1c91c53ede10c0aefd28
Mo Morsi <mmorsi@redhat.com>
2013-04-09 15:13:07 -0400

    add prelimary support to build images w/ oz and use them in the test suite

--- test/redhat_converter_test.py
+++ test/redhat_converter_test.py
@@ -1,4 +1,5 @@
-# test/converter_test.py unit test suite for guestconv convert operation
+# test/redhat_converter_test.py unit test suite for
+# guestconv redhat converter operations
 #
 # (C) Copyright 2013 Red Hat Inc.
 #
@@ -15,30 +16,30 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-
 import os
 import unittest
-import tempfile
-from xml.dom import minidom
 
 from guestconv.converter import Converter
 import guestconv.converters.redhat
 
+IMG_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'images')
+F17IMG  = os.path.join(IMG_DIR, 'f17x86_64.img')
+
 def logger(level, msg):
     print msg
 
-class ConverterTest(unittest.TestCase):
+class GrubTest(unittest.TestCase):
     def setUp(self):
         self.c = Converter(['conf/guestconv.db'], logger)
 
-    def testInspect(self):
-        drive = tempfile.NamedTemporaryFile()
-        self.c.add_drive(drive.name)
-        xml = self.c.inspect('rhev')
-        xmldoc = minidom.parseString(xml)
-        self.assertEqual(1, xmldoc.getElementsByTagName('guestconv').length)
-        drive.close()
-
-    def testConvert(self):
-        # self.c.convert('TODO')
-        self.assertTrue(1 == 1)
+    @unittest.skipUnless(os.path.exists(F17IMG), "image does not exist")
+    def testGrubListKernels(self):
+        self.c.add_drive(F17IMG)
+        self.c._h.add_drive_opts(F17IMG, format = "raw", readonly = 0)
+        self.c._h.launch()
+        self.c._h.mount('/dev/VolGroup00/LogVol00', '/')
+        self.c._h.mount('/dev/vdb2', '/boot')
+        grub = guestconv.converters.redhat.Grub2BIOS(self.c._h, '/dev/VolGroup00/LogVol00', logger)
+        kernels = grub.list_kernels()
+        self.assertEqual(1, len(kernels))
+        self.assertEqual("/boot/vmlinuz-3.3.4-5.fc17.x86_64", kernels[0])
--- test/run.py
+++ test/run.py
@@ -19,15 +19,45 @@
 
 import unittest
 
+import os
+import os.path
 import sys
+import subprocess
+import tempfile
 sys.path.append("../guestconv/")
 
 import guestconv
 import guestconv
 
 import convertertest
+import redhat_converter_test
+
+TDL_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'tdls')
+IMG_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'images')
+
+def build_tdl(tdl):
+    image   = os.path.join(IMG_DIR, tdl.replace("tdl", "img"))
+    if os.path.exists(image):
+        return
+    stdoutf = tempfile.TemporaryFile()
+    # requires root privs:
+    popen   = subprocess.Popen(["oz-install", "-s", image, os.path.join(TDL_DIR, tdl)], stdout=stdoutf, stderr=stdoutf)
+    popen.wait()
+    stdoutf.seek(0)
+    stdout = stdoutf.read()
+    stdoutf.close()
+    print stdout
+    # TODO analyze stdout/stderr/exitcode for errors
+
+    return image
+
+def build_tdls():
+    for tdl in os.listdir(TDL_DIR):
+        build_tdl(tdl)
 
 if __name__ == '__main__':
+    build_tdls()
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(convertertest.ConverterTest))
+    suite.addTest(unittest.makeSuite(redhat_converter_test.GrubTest))
     unittest.TextTestRunner(verbosity=2).run(suite)
